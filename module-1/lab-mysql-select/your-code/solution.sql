c8828f5e7a04c45a987d9d66b06bc3910c6dad85
Miguel Venegas <m.venegas318@gmail.com>
2019-11-21 21:03:24 +0100

    refinished_lab

--- module-1/lab-mysql-select/your-code/solution.sql
+++ module-1/lab-mysql-select/your-code/solution.sql
@@ -1,7 +1,65 @@
-select * from titles
-join publishers on titles.pub_id = publishers.pub_id
-join titleauthor on titleauthor.title_id = titles.title_id 
-join authors on authors.au_id = titleauthor.au_id ;
+select count ()
+FROM titleauthor;
+
+select authors.au_id AS 'AUTHOR ID',
+au_lname AS 'LAST NAME',
+au_fname AS 'FIRST NAME', titles.title AS 'TITLE', publishers.pub_name AS'PUBLISHERS'
+from authors
+JOIN titleauthor
+on titleauthor.au_id = authors.au_id
+JOIN titles
+on titleauthor.title_id = titles.title_id
+join publishers
+on publishers.pub_id = titles.pub_id;
+
+
+
+ /*Challenge 2:*/
+select authors.au_id AS 'AUTHOR ID',
+au_lname AS 'LAST NAME',
+au_fname AS 'FIRST NAME', publishers.pub_name AS'PUBLISHERS', COUNT (title) as 'TITLE COUNT'
+from authors
+left JOIN titleauthor
+on titleauthor.au_id = authors.au_id
+left JOIN titles
+on titleauthor.title_id = titles.title_id
+left join publishers
+on publishers.pub_id = titles.pub_id
+GROUP BY authors.au_id;
+
+
+/*Challenge 3:*/
+select sum (qty)
+from sales;
+
+select authors.au_id as 'AUTHOR ID',
+au_lname AS 'LAST NAME',
+au_fname AS 'FIRST NAME', sum(sales.qty) as 'TOTAL'
+from authors
+left JOIN titleauthor
+on titleauthor.au_id = authors.au_id
+left JOIN titles
+on titleauthor.title_id = titles.title_id
+left join sales
+on sales.title_id = titles.title_id
+GROUP BY authors.au_id
+ORDER BY sum(sales.qty) DESC
+limit 3;
+
+
+ /*Challenge 4:*/
+select authors.au_id as 'AUTHOR ID',
+au_lname AS 'LAST NAME',
+au_fname AS 'FIRST NAME', sum(sales.qty) as 'TOTAL'
+from authors
+left JOIN titleauthor
+on titleauthor.au_id = authors.au_id
+left JOIN titles
+on titleauthor.title_id = titles.title_id
+left join sales
+on sales.title_id = titles.title_id
+GROUP BY authors.au_id
+ORDER BY sum(sales.qty) DESC;
 
  
 
